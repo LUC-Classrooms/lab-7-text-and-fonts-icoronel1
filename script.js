be23578b25eb7521cc7f99514552472de489a76e
Isa Coronel <156938989+icoronel1@users.noreply.github.com>
2024-02-23 16:52:01 +0000

    Lab 7 completed with extra credit

--- script.js
+++ script.js
@@ -4,14 +4,29 @@
 
 function setup() {
   createCanvas(640, 240);
-  textFont("Arial");
+  textFont("Times");
 }
+
+var phrase1 = "Welcome to my problems..."
+var phrase2 = "It's not an invitation."
+var phrase = "";
 
 function draw() {
   background(200);
-  textSize(16);
-  text("one small step for a man ...", 20, 60);
-  textSize(32);
-  text("one giant leap for mankind.", 20, 160);
+  //textSize(12);
+  //textFont("Times")
+  //text("one small step for a man ...", 20, 60);
+  //textFont("Courier")
+  if(mouseIsPressed){
+    textSize(32);
+    //text("one giant leap for mankind.", 50, 140);
+    phrase = phrase2
+  } else {
+    textSize(12);
+  //textFont("Times")
+    //text("one small step for a man ...", 50, 140);
+    phrase = phrase1
+  }
+  text(phrase, 180, 120);
 
 }
